4fffa470a6e47e33f26b062de9ae9878c04fb697
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2023-06-08 23:59:02 +0300

    fix: next link href

--- ui/components/NextLink.tsx
+++ ui/components/NextLink.tsx
@@ -1,11 +1,11 @@
-import styled from "@emotion/styled";
+// @ts-nocheck
 import { Box, SxProps } from "@mui/material";
 import Link, { LinkProps } from "next/link";
 import React from "react";
 
 const NextLink = ({ children, href, sx, ...rest }: LinkProps & { children: React.ReactNode } & { sx?: SxProps }) => (
     <Link href={href} {...rest}>
-        <Box component="a" sx={{ ...sx, "& *": { cursor: "pointer" } }}>
+        <Box component="a" href={href} sx={{ ...sx, "& *": { cursor: "pointer" } }}>
             {children}
         </Box>
     </Link>
